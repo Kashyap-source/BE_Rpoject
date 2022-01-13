e26f324f7656334b1507dcceef46af79b0979e91
Kashyap Panchal <kashyap.panchal2000@gmail.com>
2022-01-13 15:22:19 +0530

    update

--- BE_project/BE_project/settings.py
+++ BE_project/BE_project/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 
 # Added manually 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static',)
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
